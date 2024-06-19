1bb87dab4eb3696d0ed372b68d2b62fe221f60e0
JavaCommons Technologies <javacommons@gmail.com>
2024-06-19 13:49:31 +0900

    .

--- EasyLanguage.Demo/Program.cs
+++ EasyLanguage.Demo/Program.cs
@@ -9,25 +9,28 @@ static class Program
     [STAThread]
     static void Main(string[] originalArgs)
     {
-        var el1 = Global.EasyLanguageParser.Parse("""
+        var el1 = EasyObject.FromJson("""
             { "a": //line comment
               123
-              b.c.d: `(777 888) }
+              b: `(777 888) }
             """);
         Echo(el1, "el1");
+        Xunit.Assert.Equal("""
+            {"a":123,"b":[":quote",[777,888]]}
+            """, el1.ToJson());
         var el2 = EasyObject.FromJson("""
             { "a": //line comment
               123
-              b.c.d: `(add2 777 888) }
+              b: `(add2 777 888) }
             """);
         Echo(el2, "el2");
         var el3 = Global.EasyLanguageParser.Parse("""
             { "a": //line comment
               123
-              b.c.d: `(add2 777 888)
+              b: `(add2 777 888)
               x: (@1+ 123)
-              y: @_.!%&-=^~+*<>/?
-              z: :keyword-_.!%&-=^~+*<>/? }
+              y: @_!%&-=^~+*<>/?
+              z: :keyword-_!%&-=^~+*<>/? }
             """);
         Echo(el3, "el3");
         var elang = new EasyLanguage();

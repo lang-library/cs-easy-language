2a4b79b8d81219e94a204f2dffb80f76d450c07f
JavaCommons Technologies <javacommons@gmail.com>
2024-06-22 10:51:48 +0900

    .

--- EasyLanguage.Demo/Program.cs
+++ EasyLanguage.Demo/Program.cs
@@ -2,6 +2,10 @@
 using System;
 using System.IO;
 using static Global.EasyObject;
+using Microsoft.ClearScript;
+using Microsoft.ClearScript.JavaScript;
+using Microsoft.ClearScript.V8;
+
 namespace Main;
 
 
@@ -40,9 +44,11 @@ static class Program
 #endif
         var ans02 = elang.EvalFile("assets/test.elAng");
         Echo(ans02, "ans02");
+#if false
         var csharp = new ELang.CSharpScript();
         var answer = csharp.Evaluate("11+22");
         Echo(answer, "answer");
+#endif
         var order = EasyObject.FromJson("{b: 123, a: 456, _:789}");
         Echo(order, "order");
         var interp = NukataLisp.MakeInterp().Result;
@@ -56,5 +62,95 @@ static class Program
         StringReader sr = new StringReader("(print (add2 11 22))");
         var r = NukataLisp.Run(interp, sr).Result;
         Echo(r, "r");
+
+        using (var engine = new V8ScriptEngine())
+
+        {
+            // expose a host type
+            engine.AddHostType("Console", typeof(Console));
+            engine.Execute("Console.WriteLine('{0} is an interesting number.', Math.PI)");
+
+            // expose a host object
+            engine.AddHostObject("random", new Random());
+            engine.Execute("Console.WriteLine(random.NextDouble())");
+
+            // expose entire assemblies
+            engine.AddHostObject("lib", new HostTypeCollection("mscorlib", "System.Core"));
+            engine.Execute("Console.WriteLine(lib.System.DateTime.Now)");
+
+            // create a host object from script
+            engine.Execute(@"
+        birthday = new lib.System.DateTime(2007, 5, 22);
+        Console.WriteLine(birthday.ToLongDateString());
+    ");
+
+            // use a generic class from script
+            engine.Execute(@"
+        Dictionary = lib.System.Collections.Generic.Dictionary;
+        dict = new Dictionary(lib.System.String, lib.System.Int32);
+        dict.Add('foo', 123);
+    ");
+
+            // call a host method with an output parameter
+            engine.AddHostObject("host", new HostFunctions());
+            engine.Execute(@"
+        intVar = host.newVar(lib.System.Int32);
+        found = dict.TryGetValue('foo', intVar.out);
+        Console.WriteLine('{0} {1}', found, intVar);
+    ");
+
+            // create and populate a host array
+            engine.Execute(@"
+        numbers = host.newArr(lib.System.Int32, 20);
+        for (var i = 0; i < numbers.Length; i++) { numbers[i] = i; }
+        Console.WriteLine(lib.System.String.Join(', ', numbers));
+    ");
+
+            // create a script delegate
+            engine.Execute(@"
+        Filter = lib.System.Func(lib.System.Int32, lib.System.Boolean);
+        oddFilter = new Filter(function(value) {
+            return (value & 1) ? true : false;
+        });
+    ");
+
+            // use LINQ from script
+            engine.Execute(@"
+        oddNumbers = numbers.Where(oddFilter);
+        Console.WriteLine(lib.System.String.Join(', ', oddNumbers));
+    ");
+
+            // use a dynamic host object
+            engine.Execute(@"
+        expando = new lib.System.Dynamic.ExpandoObject();
+        expando.foo = 123;
+        expando.bar = 'qux';
+        delete expando.foo;
+    ");
+
+            // call a script function
+            engine.Execute("function print(x) { Console.WriteLine(x); }");
+            engine.Script.print(DateTime.Now.DayOfWeek);
+
+            // examine a script object
+            engine.Execute("person = { name: 'Fred', age: 5 }");
+            Console.WriteLine(engine.Script.person.name);
+
+            // read a JavaScript typed array
+            engine.Execute("values = new Int32Array([1, 2, 3, 4, 5])");
+            var values = (ITypedArray<int>)engine.Script.values;
+            Console.WriteLine(string.Join(", ", values.ToArray()));
+
+            // EasyObject
+            engine.AddHostType("EasyObject", typeof(EasyObject));
+            var eo = EasyObject.FromObject(new []  { 1, 2, 3 });
+            engine.AddHostObject("eo", eo);
+            engine.Execute("""
+                function echo(msg, title = null) { EasyObject.Echo(msg, title); }
+                EasyObject.Echo(eo, "eo");
+                EasyObject.Echo(eo[1], "eo[1]");
+                EasyObject.Echo(eo[1]);
+                """);
+        }
     }
 }

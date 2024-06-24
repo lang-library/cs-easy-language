bc4419843ce5f1ba74bad84e0fdf9d1ab6df888e
JavaCommons Technologies <javacommons@gmail.com>
2024-06-24 22:52:07 +0900

    .

--- EasyLanguage.Demo/assets/test.el
+++ EasyLanguage.Demo/assets/test.el
@@ -3,9 +3,9 @@
  (define sum (add 11 (add 22 33 44)))
  ([. Console WriteLine] sum)
  (Echo '"hello")
- $ Echo("as-is$$") $
- (define map {a: (add 11 22) b: $33+44$})
- $ Echo(map) $
- #!sum
- $ undefined $
+ $@ Echo("as-is@@") @
+ (define map {a: (add 11 22) b: $@33+44@})
+ $@ Echo(map) @
+ #sum
+ $@ undefined @
  )
